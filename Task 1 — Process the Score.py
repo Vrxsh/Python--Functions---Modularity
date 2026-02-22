20f879f4d571385417c63879df9825d6b07cc325
Vasanth G <vasanth.gganesan@gmail.com>
2026-02-22 09:06:29 +0530

    final

--- Task 1 — Process the Score.py
+++
@@ -1,6 +0,0 @@
-def process_scores(students):
-    averages = {}
-    for name, scores in students.items():
-        avg = round(sum(scores) / len(scores), 2)
-        averages[name] = avg
-    return averages
